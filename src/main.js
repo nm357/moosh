4d962059a6c0f6851d49097c72982c85a89fb539
n m <nm357@users.noreply.github.com>
2020-08-10 14:26:40 -0400

    render Moosh Land

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import App from './App.vue'
-import Home from './components/Home.vue'
+import MegaMooshLand from './components/MegaMooshLand.vue'
 
 Vue.use(VueRouter)
 
@@ -11,7 +11,7 @@ Vue.config.productionTip = false
 const mooshRouter = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/home', component: Home }
+    { path: '/', component: MegaMooshLand }
   ]
 })
 
