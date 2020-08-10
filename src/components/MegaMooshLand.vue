4aa529252b6cae53e1b0a37a29712934e98726ae
n m <nm357@users.noreply.github.com>
2020-08-10 14:26:19 -0400

    make Mega Moosh Land home!

---
+++ src/components/MegaMooshLand.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div id="world">
+
+      <div id="verticalSpace">
+        <img src="../assets/greenshoes.png" alt="8-bit Greenshoes" sizes="" srcset="">
+
+      </div>
+
+      <div id="ground">
+        <img src="../assets/moosh-brick.png" alt="Brick Tile" width="128px">
+      </div>
+
+      <div id="background">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import WorldTile from './WorldTile.tsx';
+
+export default {
+  name: 'MegaMooshLand',
+};
+</script>
