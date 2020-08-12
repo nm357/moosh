2945fa95196de1da45fb553fe61c3aa110414093
n m <nm357@users.noreply.github.com>
2020-08-12 16:47:12 -0400

    wip world

--- src/components/MegaMooshLand.vue
+++ src/components/MegaMooshLand.vue
@@ -3,12 +3,14 @@
     <div id="world">
 
       <div id="verticalSpace">
+
         <img src="../assets/greenshoes.png" alt="8-bit Greenshoes" sizes="" srcset="">
 
       </div>
 
       <div id="ground">
-        <img src="../assets/moosh-brick.png" alt="Brick Tile" width="128px">
+        <!-- <img src="../assets/moosh-brick.png" alt="Brick Tile" width="128px"> -->
+        {{ ground }}
       </div>
 
       <div id="background">
@@ -18,10 +20,33 @@
   </div>
 </template>
 
-<script>
-// import WorldTile from './WorldTile.tsx';
+<script lang="tsx">
+import WorldTile from './WorldTile';
+
+const ground = () => {
+  try {
+    const cornerstone: WorldTile = new WorldTile(
+      '../assets/moosh-brick.png', 'Brick Tile');
+    // return new Array(Math.ceil(this.props.windowWidth / cornerstone.offsetWidth)).map((tile: HTMLElement) => tile = cornerstone.cloneNode());
+    return new Array(Math.ceil(this.props.windowWidth / cornerstone.renderElement().offsetWidth)).map((tile) => {
+      return Object.assign(tile, cornerstone);
+    });
+  } catch (error) {
+    console.log('Error while building ground: ', error);
+    return ['ground'];
+  }
+}
 
 export default {
   name: 'MegaMooshLand',
+  props: {
+    windowHeight: Number,
+    windowWidth: Number
+  },
+  data: function() {
+    return {
+      ground
+    }
+  }
 };
 </script>
