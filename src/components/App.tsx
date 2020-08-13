b6ab5ced7f47c68f3e1c249492bc7a84a1df943a
n m <nm357@users.noreply.github.com>
2020-08-13 00:21:42 -0400

    fix image imports; tidy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 
+import MegaMooshLand from './MegaMooshLand';
+
+import githubLogo from '../assets/GitHub_Logo.png';
+import linkedinLogo from '../assets/LI-Logo.png';
+import greenshoes from '../assets/greenshoes.png';
+import greenshoesJumping from '../assets/greenshoes-jumping.png';
+
 export default class App extends React.PureComponent {
   private currentYear: number = new Date().getFullYear();
+
+  private footerSection = (
+    <section id="footer">
+      Nicholas Mikita, { `${this.currentYear}` } 
+      <a target="_blank" href="githubUrl">
+        <img src={githubLogo} width="60px" />
+      </a>
+      <a target="_blank" href="linkedinUrl">
+        <img src={linkedinLogo} width="60px" />
+      </a>
+    </section>
+  );
+
+  private linksSection = (
+    <section id="links">
+      <div><a href="/art">Art</a></div>
+      <div><a href="/philosophy">Philosophy</a></div>
+      <div><a href="/language">Language</a></div>
+      <div><a href="/software">Software</a></div>
+    </section>
+  );
 
   render() {
     return(
       <div>
-        <h1>Welcome to Mega Moosh Land! 🖖</h1>
-       
-        <section id="links">
-          <div><a href="/art">Art</a></div>
-          <div><a href="/philosophy">Philosophy</a></div>
-          <div><a href="/language">Language</a></div>
-          <div><a href="/software">Software</a></div>
+        
+        { this.linksSection }
+
+        <section id="world">
+          <h1>🖖 Welcome to Mega Moosh Land! 🖖</h1>
+          <MegaMooshLand />
         </section>
       
-      <div id="footer">
-        Nicholas Mikita, { `${this.currentYear}` } 
-        <a target="_blank" href="githubUrl">
-          <img src="./assets/GitHub_Logo.png" width="60px" />
-        </a>
-        <a target="_blank" href="linkedinUrl">
-          <img src="./assets/LI-Logo.png" width="60px" />
-        </a>
-      </div>
+        { this.footerSection }
     </div>
     )
   }
